65beb2282fa29fcfbf48a743d5de66d4f5b9c3f8
HamzaZaazaa <Hamza.zaazaa01@hotmail.com>
2023-02-12 14:48:01 +0100

    added few framer motion animations

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import CategoryCard from "./CategoryCard";
 import "../categories/categories.css";
-import { Spinner } from "react-bootstrap";
+import { Button, Card, Spinner } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 function Categories() {
   const [categories, setCategories] = useState([]);
   let [loading, setLoading] = useState(true);
+
   useEffect(() => {
     axios
       .get("https://api.chucknorris.io/jokes/categories")
       .then((res) => setCategories(res.data))
-      // .then(() => categories.push("uncategorized"))
       .finally(() => setLoading((loading = false)))
       .catch((err) => console.log(err));
   }, [categories]);
@@ -22,10 +23,50 @@ function Categories() {
       <div className='mappedCards'>
         {!loading ? (
           newArr.map((cat) => {
-            return <CategoryCard cat={cat} key={cat} />;
+            return (
+              <motion.div
+                className='mt-3'
+                animate={{ x: 20 }}
+                transition={{ ease: "easeOut", duration: 2 }}
+              >
+                <Card
+                  style={{ width: "10rem", boxShadow: "4px 2px aqua" }}
+                  bg='dark'
+                  text='light'
+                  border='info'
+                >
+                  <Card.Body>
+                    <Card.Title
+                      style={{
+                        fontWeight: "bold",
+                        textTransform: "uppercase",
+                        fontFamily: "cursive",
+                      }}
+                    >
+                      <h6 style={{ display: "flex", justifyContent: "center" }}>
+                        {cat}
+                      </h6>
+                    </Card.Title>
+                    <Link to={`/jokes/${cat}`}>
+                      <motion.div
+                        whileHover={{ scale: 1.5 }}
+                        whileTap={{ scale: 0.9 }}
+                      >
+                        <Button variant='outline-info'>Jokes</Button>
+                      </motion.div>
+                    </Link>
+                  </Card.Body>
+                </Card>
+              </motion.div>
+            );
           })
         ) : (
-          <Spinner animation='border' role='status'>
+          <Spinner
+            animation='border'
+            role='status'
+            variant='info'
+            style={{ marginTop: "10%" }}
+          >
             <span className='visually-hidden'>Loading...</span>
           </Spinner>
         )}
